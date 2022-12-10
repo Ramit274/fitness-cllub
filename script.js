9fcb4acf7a43e99fe3062bd13a3d3b4ff107edb8
Ramit <113528976+Ramit274@users.noreply.github.com>
2022-12-10 13:43:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayalert(){
+    window.alert('How you Doin?')
+}
+function displayalerta(){
+    document.getElementById('container').classList.add('dvc');
+}
+function displayalertb(){
+    document.getElementById('container').classList.remove('dvc');
+}
